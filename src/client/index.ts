efde2c449fa8063807180356e235a0e61e5951d2
Code Padawan <lucianjr.serbanescu@icloud.com>
2026-05-18 18:32:13 +0300

    CH3 L7

--- src/client/index.ts
+++ src/client/index.ts
@@ -13,45 +13,67 @@ import { commandSpawn } from "../internal/gamelogic/spawn.js";
 import { commandMove } from "../internal/gamelogic/move.js";
 
 async function main() {
-  console.log("Starting Peril client...");
-  const connection = "amqp://guest:guest@localhost:5672/";
-  const conn = await amqp.connect(connection);
-  console.log("Connected to RabbitMQ");
+  const rabbitConnString = "amqp://guest:guest@localhost:5672/";
+  const conn = await amqp.connect(rabbitConnString);
+  console.log("Peril game client connected to RabbitMQ!");
+
+  ["SIGINT", "SIGTERM"].forEach((signal) =>
+    process.on(signal, async () => {
+      try {
+        await conn.close();
+        console.log("RabbitMQ connection closed.");
+      } catch (err) {
+        console.error("Error closing RabbitMQ connection:", err);
+      } finally {
+        process.exit(0);
+      }
+    }),
+  );
 
   const username = await clientWelcome();
 
   await declareAndBind(
     conn,
     ExchangePerilDirect,
-    `pause.${username}`,
+    `${PauseKey}.${username}`,
     PauseKey,
     SimpleQueueType.Transient,
   );
 
-  const gameState = new GameState(username);
+  const gs = new GameState(username);
 
   while (true) {
     const words = await getInput();
-
-    if (words.length === 0) continue;
-
+    if (words.length === 0) {
+      continue;
+    }
     const command = words[0];
-    if (command === "spawn") {
-      console.log("Spawing smth");
+    if (command === "move") {
       try {
-        await commandSpawn(gameState, words);
+        commandMove(gs, words);
       } catch (err) {
-        console.error("Error publishing pause message:", err);
+        console.log((err as Error).message);
       }
-    } else if (command === "resume") {
+    } else if (command === "status") {
+      commandStatus(gs);
+    } else if (command === "spawn") {
+      try {
+        commandSpawn(gs, words);
+      } catch (err) {
+        console.log((err as Error).message);
+      }
+    } else if (command === "help") {
+      printClientHelp();
+    } else if (command === "quit") {
+      printQuit();
+      process.exit(0);
+    } else if (command === "spam") {
+      console.log("Spamming not allowed yet!");
+    } else {
+      console.log("Unknown command");
+      continue;
+    }
   }
-
-  process.on("SIGINT", async () => {
-    console.log("Got SIGINT signal.");
-    // process.kill(process.pid, "SIGINT");
-    await conn.close();
-    process.exit(0);
-  });
 }
 
 main().catch((err) => {
